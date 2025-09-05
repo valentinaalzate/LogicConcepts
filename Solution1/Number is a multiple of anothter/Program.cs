92bdfdda3585d22bae010b144d108b8d7b3b595b
valentinaalzate <vaev39@gmail.com>
2025-09-04 19:46:58 -0500

    Is multiple of nother

---
+++ Solution1/Number is a multiple of anothter/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+
+
+
+do
+{
+    var a = ConsoleExtension.GetInt("ingrese primer numero:");
+    var b = ConsoleExtension.GetInt("ingrese segundo numero:");
+
+    if (b % a == 0)
+    {
+        Console.WriteLine($"El numero {a} es multiplo de  {b}");
+    }
+    else {
+        Console.WriteLine($"{a} no es  multiplo de {b}");
+    }
+
+
+} while (true);
+
