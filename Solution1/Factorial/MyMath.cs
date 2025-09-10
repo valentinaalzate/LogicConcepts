397914c5f9280ce8d4376ec699ffa8be0555e3cb
valentinaalzate <vaev39@gmail.com>
2025-09-10 07:37:34 -0500

    factorial

---
+++ Solution1/Factorial/MyMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    public class MyMath
+    {
+        public static double Factorial(int n) {
+
+            double factorial = 1;
+
+            for (int i=2; i<=n; i++) {
+
+                factorial *= i;
+
+            
+            }
+            return factorial;
+
+        }
+    }
+}
---
+++ Solution1/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using Factorial;
+using Shared;
+using System.ComponentModel.Design;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInter("Ingrese numero: ");
+    var factorial = MyMath.Factorial(n);
+    Console.WriteLine($"{n}! = {factorial:n0}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]0?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
